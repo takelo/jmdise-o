ef6dd208cf4ba6f766fc9aa849a23746d7ff4f6e
Juan Manuel Echeverria <factorzero@gmail.com>
2022-04-06 17:07:19 -0300

    colorized

--- js/game.js
+++ js/game.js
@@ -67,7 +67,7 @@ $(document).ready(function () {
   const x = canvas.width / 2;
   const y = canvas.height / 2;
 
-  const player = new Player(x, y, 30, "blue");
+  const player = new Player(x, y, 10, "white");
 
   // const projectile = new Projectile(
   //   canvas.width / 2,
@@ -97,7 +97,7 @@ $(document).ready(function () {
         x = Math.random() * canvas.width;
         y = Math.random() < 0.5 ? 0 - radius : canvas.height + radius;
       }
-      const color = "green";
+      const color = `hsl(${Math.random() * 360}, 50%, 50%)`;
       const angle = Math.atan2(canvas.height / 2 - y, canvas.width / 2 - x);
       const velocity = {
         x: Math.cos(angle),
@@ -112,10 +112,21 @@ $(document).ready(function () {
   let animationId;
   function animate() {
     animationId = requestAnimationFrame(animate);
-    c.clearRect(0, 0, canvas.width, canvas.height);
+    c.fillStyle = 'rgb(0,0,0, 0.1)';
+    c.fillRect(0, 0, canvas.width, canvas.height);
     player.draw();
-    projectiles.forEach((projectile) => {
+    projectiles.forEach((projectile, index) => {
       projectile.update();
+
+      // remove proyerctiles out view
+      if(projectile.x + projectile.radius < 0 ||
+         projectile.x - projectile.radius > canvas.width ||
+         projectile.y + projectile.radius < 0 ||
+         projectile.y - projectile.radius > canvas.height){
+        setTimeout(() => {
+          projectiles.splice(index, 1);
+        }, 0);
+      }
     });
     enemies.forEach((enemy, index) => {
       enemy.update();
@@ -141,14 +152,18 @@ $(document).ready(function () {
   }
 
   window.addEventListener("click", (e) => {
+    console.log(projectiles);
     const angle = Math.atan2(event.clientY - canvas.height / 2, event.clientX - canvas.width / 2);
     const velocity = {
-      x: Math.cos(angle),
-      y: Math.sin(angle),
+      x: Math.cos(angle)*5,
+      y: Math.sin(angle)*5,
     };
-    projectiles.push(new Projectile(canvas.width / 2, canvas.height / 2, 5, "red", velocity));
+    projectiles.push(new Projectile(canvas.width / 2, canvas.height / 2, 5, "white", velocity));
   });
 
   animate();
   spawnEnemies();
 }); //lave document ready
+
+
+// time 1:11
