21b398caf2cd0aa305830693de6668fc4e223aff
Juan Manuel Echeverria <factorzero@gmail.com>
2022-11-22 13:28:45 -0300

    scroll animation test

--- js/mis_jquery.js
+++ js/mis_jquery.js
@@ -77,6 +77,36 @@ $(document).ready(function() {
 //  });
 
 
+const triggers = [
+  { name: "first", from: 100, to: 375 },
+  { name: "second", from: 400, to: 675 },
+  { name: "third", from: 600, to: 850 }
+];
+
+console.log(triggers);
+function animationPercentage(trigger, scrollY) {
+  if (scrollY < trigger.from) return 0;
+  if (scrollY > trigger.to) return 1;
+  return (scrollY - trigger.from) / (trigger.to - trigger.from);
+}
+
+function updateTriggers(e) {
+  triggers.forEach(trigger => {
+    /* To support more complex easing, you could set multiple properties,
+     * each one corresponding to a different timing function.
+     * E.g. --${trigger.name}-animation-ease-in-out
+     * and you would compute the percentage based on the linear animation percentage used here
+     */
+    const property = `--${trigger.name}-animation`;
+    document.documentElement.style.setProperty(
+      property,
+      animationPercentage(trigger, window.scrollY)
+    );
+  });
+}
+
+window.addEventListener("scroll", updateTriggers);
+updateTriggers();
 
 
 });//lave document ready
